9a6b1b0e256e26ec682a5af02a44159d82f66e14
griffin-and-co <1griffinharrison@gmail.com>
2023-12-10 20:16:41 -0500

    reviews

---
+++ CT/Reviews.swift
@@ -0,0 +1,18 @@
+//
+//  Reviews.swift
+//  CT
+//
+//  Created by Griffin Harrison on 12/10/23.
+//
+
+import SwiftUI
+
+struct Reviews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Reviews()
+}
