be5e425f147933cdc292d5b805c9169e1d9ee7f1
griffin-and-co <1griffinharrison@gmail.com>
2023-12-10 20:06:50 -0500

    ratio

--- CT/ContentView.swift
+++ CT/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("griff")
+            Text("joe burrow")
         }
         .padding()
     }
