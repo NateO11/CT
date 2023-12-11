60c1bac0e267b6baa5afd5190cb0803297934194
NateO11 <ndo1503@gmail.com>
2023-12-10 19:29:38 -0500

    Initial Commit

---
+++ CT/CTApp.swift
@@ -0,0 +1,17 @@
+//
+//  CTApp.swift
+//  CT
+//
+//  Created by Nate Owen on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
