916d9e123d5238c47aaa1256e6a30945349afab7
NateO11 <ndo1503@gmail.com>
2023-12-14 20:28:03 -0500

    signin and signup

--- CT/Screens/LoginPage.swift
+++ CT/Screens/LoginPage.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 import Firebase
 
-
 struct LoginPage: View {
     @State private var username: String = ""
     @State private var password: String = ""
@@ -17,71 +16,69 @@ struct LoginPage: View {
     @State private var login: Bool = false
 
     var body: some View {
-        VStack {
-            Text("Login")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .multilineTextAlignment(.center)
-                .padding()
-            
-            Image(systemName: "person.circle.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 100, height: 100)
-                .padding(.bottom, 30)
-            
-            TextField("Email", text: $username)
-                .padding()
-                .background(Color(.systemGray6))
-                .cornerRadius(10)
-                .padding()
-                .shadow(radius: 10)
-
-            
-            TextField("Password", text: $password)
-                .padding()
-                .background(Color(.systemGray6))
-                .cornerRadius(10)
-                .padding(.bottom, 20)
-                .padding()
-                .shadow(radius: 10)
-
-            
-            Button(action: { validateUser()}) {
+        // NavigationView moved to the top
+        NavigationView {
+            VStack {
                 Text("Login")
-                    .font(.title)
-                    .frame(width: 200, height: 60)
-                    .background(Color.black)
-                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    .padding()
+
+                Image("CTlogo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
+                    .padding(.bottom, 30)
+
+                TextField("Email", text: $username)
+                    .padding()
+                    .background(Color(.systemGray6))
                     .cornerRadius(10)
-                    .shadow(radius: 5)
-        }
-            
-            Text("New User? Sign Up Here")
-                                .foregroundColor(.blue)
-                                .padding()
-                                .onTapGesture {
-                                    // navigate to login screen
-                                }
-        .padding()
-        }
-        .padding()
-    }
-    func validateUser(){
-            Auth.auth().signIn(withEmail: email, password: password) { result, error in
-                // At this point, our user is signed in
-                signedIn = true
+                    .padding()
+                    .shadow(radius: 10)
+
+                TextField("Password", text: $password)
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                    .padding(.bottom, 20)
+                    .padding()
+                    .shadow(radius: 10)
+
+                Button(action: { validateUser() }) {
+                    Text("Login")
+                        .font(.title)
+                        .frame(width: 200, height: 60)
+                        .background(Color.black)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+                }
                 
-             
+                // NavigationLink at the bottom
+                NavigationLink(destination: SignUp()) {
+                    Text("New User? Sign Up Here")
+                        .foregroundColor(.blue)
+                        .padding()
+                }
+                .padding()
+            }
+            .padding()
+        }
+        .navigationBarHidden(true)
+
+    }
+    
+
+    func validateUser() {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            // At this point, our user is signed in
+            signedIn = true
         }
     }
 }
 
-
-
-
-struct LoginPage_Previews: PreviewProvider {
-    static var previews: some View {
-        LoginPage()
-    }
+#Preview {
+    LoginPage()
 }
--- CT/Screens/SignUp.swift
+++ CT/Screens/SignUp.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 import Firebase
 
-
 struct SignUp: View {
     @State private var username: String = ""
     @State private var password: String = ""
@@ -16,8 +15,9 @@ struct SignUp: View {
     @State private var confirmPassword: String = ""
     @State private var signedIn: Bool = false
     
-    
     var body: some View {
+        // NavigationView moved to the top
+        NavigationView {
             VStack {
                 Text("Sign Up")
                     .font(.largeTitle)
@@ -25,99 +25,101 @@ struct SignUp: View {
                     .multilineTextAlignment(.center)
                     .padding()
                 
-                Image(systemName: "person.circle.fill")
+                Image("CTlogo")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 100, height: 100)
                     .padding(.bottom, 30)
-                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+
                 
                 TextField("Username", text: $username)
                     .padding()
                     .background(Color(.systemGray6))
                     .cornerRadius(10)
-                    .padding(.bottom, 20)
+                    .padding()
                     .shadow(radius: 10)
                 
                 TextField("Email", text: $email)
                     .padding()
                     .background(Color(.systemGray6))
                     .cornerRadius(10)
-                    .padding(.bottom, 20)
-                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                    .padding()
+                    .shadow(radius: 10)
                 
                 TextField("Password", text: $password)
                     .padding()
                     .background(Color(.systemGray6))
                     .cornerRadius(10)
-                    .padding(.bottom, 20)
-                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                    .padding()
+                    .shadow(radius: 10)
                 
                 TextField("Confirm Password", text: $confirmPassword)
                     .padding()
                     .background(Color(.systemGray6))
                     .cornerRadius(10)
                     .padding(.bottom, 20)
-                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                    .shadow(radius: 10)
+                    .padding()
                 
-            }
-            .padding()
-            Button(action: {
-                createUser()
-                
-            }) {
-                Text("Sign Up")
-                    .font(.title)
-                    .frame(width: 200, height: 60)
-                    .background(Color.black)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-                    .shadow(radius: 5)
-            }
-            Text("Existing User? Sign In Here")
-                .foregroundColor(.blue)
-                .padding()
-                .onTapGesture {
-                   // LoginPage()
+                Button(action: {
+                    createUser();
+                }) {
+                            Text("Sign Up")
+                                .font(.title)
+                                .frame(width: 200, height: 60)
+                                .background(Color.black)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                                .shadow(radius: 5)
                 }
-}
-    
-    func createUser(){
-        if (email != "" && password != "" && username != "" && confirmPassword != ""){
-            if(confirmPassword == password){
-                Auth.auth().createUser(withEmail: email, password: password){ (res, err) in
-                    if err != nil{
+                
+                NavigationLink(destination: LoginPage()) {
+                    Text("Existing User? Sign In Here")
+                        .foregroundColor(.blue)
+                        .padding()
+                }
+                .padding()
+            }
+        }
+        .navigationBarHidden(true)
+    }
+
+    func createUser() {
+        if (email != "" && password != "" && username != "" && confirmPassword != "") {
+            if confirmPassword == password {
+                Auth.auth().createUser(withEmail: email, password: password) { (res, err) in
+                    if err != nil {
                         print("\(String(describing: err))")
                         return
-                    }
-                    else{
+                    } else {
                         createUserInfo()
                     }
                 }
             }
         }
-        func createUserInfo() {
-            
-            let db = Firestore.firestore()
-            let userEmail = Auth.auth().currentUser?.email ?? ""
-            let userID = Auth.auth().currentUser?.uid
-            
-            // variable name
-            let userRef = db.collection("Users").document(userID!)
-                
-                // Update existing document or create a new one
-                userRef.setData(["UserEmail": userEmail,"Username": username, "Password": password, "UserID": userID!]) { error in
-                    if error != nil {
-                        print("Error updating document")
-                    } else {
-                        print("Document successfully written or updated.")
-                        signedIn = true
-                    }
-                }
+    }
+
+    func createUserInfo() {
+        let db = Firestore.firestore()
+        let userEmail = Auth.auth().currentUser?.email ?? ""
+        let userID = Auth.auth().currentUser?.uid
+
+        // Variable name
+        let userRef = db.collection("Users").document(userID!)
+
+        // Update existing document or create a new one
+        userRef.setData(["UserEmail": userEmail, "Username": username, "Password": password, "UserID": userID!]) { error in
+            if error != nil {
+                print("Error updating document")
+            } else {
+                print("Document successfully written or updated.")
+
+                self.signedIn = true
+                   
             }
-            
         }
     }
+}
 
 
 
