d64aa3418a16e8bdfd62a5e7e5c9d3f61d54cba1
griffin-and-co <1griffinharrison@gmail.com>
2024-01-12 14:25:12 -0500

    Changed around review UI and consolidated all old code into a single page for reference if needed

---
+++ CT/Screens/New Screens/OldVersionsOfScreens.swift
@@ -0,0 +1,213 @@
+//
+//  OldVersionsOfScreens.swift
+//  CT
+//
+//  Created by Griffin Harrison on 1/12/24.
+//
+
+import Foundation
+import SwiftUI
+import MapKit
+
+
+// Old version of locationDetailView ...  please keep for reference
+/* struct LocationDetailView: View {
+    @ObservedObject var locationViewModel: LocationViewModel
+    let location: Location
+    @State private var showingReviewSheet = false
+    @Environment(\.presentationMode) var presentationMode
+
+
+    var body: some View {
+        VStack {
+            ZStack {
+                HStack {
+                    /* Button("") {
+                    }
+                    .buttonStyle(CategoryButton(category: location.category, dimensions: 12))
+                    .padding(.trailing, 10)
+                    Spacer()
+                     */
+                    Text(location.name)
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .lineLimit(2)
+                }
+                HStack {
+                    
+                }
+                HStack {
+                    Spacer()
+                    Button("") {
+                        self.presentationMode.wrappedValue.dismiss()
+                        // this should dismiss the sheet
+                    }
+                        .buttonStyle(xButton())
+                        .padding(.leading, 10)
+                    
+                }
+            }
+            .padding()
+                
+            
+            List(locationViewModel.reviews) { review in
+                ReviewView(review: review)
+            }
+            .listStyle(PlainListStyle())
+            .padding(.horizontal, -20)
+            .padding(.top, 20)
+            Button("Write a Review") {
+                showingReviewSheet = true
+            }
+            .frame(width: 160, height: 60)
+            .background(Color.blue.opacity(0.5))
+            .foregroundColor(.white)
+            .cornerRadius(40)
+            .padding()
+            .sheet(isPresented: $showingReviewSheet) {
+                WriteReviewView(isPresented: $showingReviewSheet) { rating, text in
+                    locationViewModel.submitReview(rating: rating, text: text, forLocation: location.id)
+                }
+                .presentationDetents([.medium])
+            }
+        }
+        .background(
+            RoundedRectangle(cornerRadius: 15)
+                .fill(LinearGradient(gradient: Gradient(colors: [Color.blue, Color.white]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                .opacity(0.8)
+        )
+        .onAppear {
+            locationViewModel.fetchReviews(forLocation: location.id)
+        }
+    }
+}
+ 
+ */
+
+
+/* Old review view version ... keep for reference
+struct OldReviewView: View {
+    let review: Review
+
+    var body: some View {
+        // Layout for displaying the review
+        VStack(alignment: .leading, spacing: 5) {
+            HStack {
+                Text("\(review.userID)")
+                    .font(.headline)
+                    .foregroundColor(.blue)
+                Spacer()
+                Text("\(review.rating) stars")
+                    .font(.subheadline)
+                    .foregroundColor(.yellow)
+            }
+            Text(review.text)
+                .font(.body)
+                .foregroundColor(.black)
+        }
+        .padding(10)
+        .background(Color.clear)
+        .padding(.vertical, 5)
+    }
+}
+
+
+struct NewReviewView: View {
+    let review: LocationReview
+
+    var body: some View {
+        // Layout for displaying the review
+        VStack(alignment: .leading, spacing: 5) {
+            HStack {
+                Text("\(review.userID)")
+                    .font(.headline)
+                    .foregroundColor(.blue)
+                Spacer()
+                Text("\(review.rating) stars")
+                    .font(.subheadline)
+                    .foregroundColor(.yellow)
+            }
+            Text(review.title)
+                .font(.subheadline)
+                .fontWeight(.bold)
+                .foregroundStyle(Color.black)
+            Text(review.text)
+                .font(.body)
+                .foregroundColor(.black)
+        }
+        .padding(.horizontal, 25)
+        .background(Color.clear)
+        .padding(.vertical, 5)
+    }
+}
+*/
+
+
+/*
+struct OldWriteReviewView: View {
+    @Binding var isPresented: Bool
+    @State private var rating: Int = 0
+    @State private var titleText: String = ""
+    @State private var reviewText: String = ""
+    var onSubmit: (Int, String, String) -> Void
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                ZStack {
+                    HStack {
+                        Spacer()
+                    }
+                    HStack {
+                        Text("Write review")
+                            .font(.title
+                                .bold())
+                    }
+                    HStack {
+                        Spacer()
+                        Button("") {
+                            isPresented = false
+                        }
+                            .buttonStyle(xButton())
+                            .shadow(radius: 10)
+                        
+                    }
+                }
+                .padding()
+                HStack {
+                    ForEach(0..<5) {value in
+                        Image(systemName: "star.fill")
+                            .resizable()
+                            .frame(width: 30,height: 30)
+                            .foregroundColor(self.rating >= value ? .yellow : .gray)
+                            .onTapGesture {
+                                self.rating = value
+                            }
+                    }
+                }
+                TextEditor(text: $titleText)
+                    .frame(minHeight: 20)
+                    .border(Color.black)
+                    .padding()
+                
+                TextEditor(text: $reviewText)
+                    .frame(minHeight: 200)
+                    .border(Color.black)
+                    .padding()
+                   
+                
+                Button("Submit") {
+                    onSubmit(rating + 1, titleText, reviewText)
+                    isPresented = false
+                }
+                .frame(width: 160, height: 60)
+                .background(Color.blue.opacity(0.5))
+                .foregroundColor(.white)
+                .cornerRadius(40)
+                .padding()
+            }
+        }
+    }
+}
+ */
--- CT/Screens/New Screens/SwiftUIViews.swift
+++ CT/Screens/New Screens/SwiftUIViews.swift
@@ -152,80 +152,6 @@ struct subTitleText: View {
     }
 }
 
-// Old version of locationDetailView ...  please keep for reference
-/* struct LocationDetailView: View {
-    @ObservedObject var locationViewModel: LocationViewModel
-    let location: Location
-    @State private var showingReviewSheet = false
-    @Environment(\.presentationMode) var presentationMode
-
-
-    var body: some View {
-        VStack {
-            ZStack {
-                HStack {
-                    /* Button("") {
-                    }
-                    .buttonStyle(CategoryButton(category: location.category, dimensions: 12))
-                    .padding(.trailing, 10)
-                    Spacer()
-                     */
-                    Text(location.name)
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .lineLimit(2)
-                }
-                HStack {
-                    
-                }
-                HStack {
-                    Spacer()
-                    Button("") {
-                        self.presentationMode.wrappedValue.dismiss()
-                        // this should dismiss the sheet
-                    }
-                        .buttonStyle(xButton())
-                        .padding(.leading, 10)
-                    
-                }
-            }
-            .padding()
-                
-            
-            List(locationViewModel.reviews) { review in
-                ReviewView(review: review)
-            }
-            .listStyle(PlainListStyle())
-            .padding(.horizontal, -20)
-            .padding(.top, 20)
-            Button("Write a Review") {
-                showingReviewSheet = true
-            }
-            .frame(width: 160, height: 60)
-            .background(Color.blue.opacity(0.5))
-            .foregroundColor(.white)
-            .cornerRadius(40)
-            .padding()
-            .sheet(isPresented: $showingReviewSheet) {
-                WriteReviewView(isPresented: $showingReviewSheet) { rating, text in
-                    locationViewModel.submitReview(rating: rating, text: text, forLocation: location.id)
-                }
-                .presentationDetents([.medium])
-            }
-        }
-        .background(
-            RoundedRectangle(cornerRadius: 15)
-                .fill(LinearGradient(gradient: Gradient(colors: [Color.blue, Color.white]), startPoint: .topLeading, endPoint: .bottomTrailing))
-                .opacity(0.8)
-        )
-        .onAppear {
-            locationViewModel.fetchReviews(forLocation: location.id)
-        }
-    }
-}
- 
- */
 
 struct LocationDetailView: View {
     @ObservedObject var locationViewModel: LocationViewModel
@@ -371,63 +297,6 @@ struct SubCategoryButton: View {
 }
 
 
-/* Old review view version ... keep for reference
-struct OldReviewView: View {
-    let review: Review
-
-    var body: some View {
-        // Layout for displaying the review
-        VStack(alignment: .leading, spacing: 5) {
-            HStack {
-                Text("\(review.userID)")
-                    .font(.headline)
-                    .foregroundColor(.blue)
-                Spacer()
-                Text("\(review.rating) stars")
-                    .font(.subheadline)
-                    .foregroundColor(.yellow)
-            }
-            Text(review.text)
-                .font(.body)
-                .foregroundColor(.black)
-        }
-        .padding(10)
-        .background(Color.clear)
-        .padding(.vertical, 5)
-    }
-}
-
-
-struct NewReviewView: View {
-    let review: LocationReview
-
-    var body: some View {
-        // Layout for displaying the review
-        VStack(alignment: .leading, spacing: 5) {
-            HStack {
-                Text("\(review.userID)")
-                    .font(.headline)
-                    .foregroundColor(.blue)
-                Spacer()
-                Text("\(review.rating) stars")
-                    .font(.subheadline)
-                    .foregroundColor(.yellow)
-            }
-            Text(review.title)
-                .font(.subheadline)
-                .fontWeight(.bold)
-                .foregroundStyle(Color.black)
-            Text(review.text)
-                .font(.body)
-                .foregroundColor(.black)
-        }
-        .padding(.horizontal, 25)
-        .background(Color.clear)
-        .padding(.vertical, 5)
-    }
-}
-*/
-
 struct EvenNewerReviewView: View {
     let review: LocationReview
     let firstChar: String
@@ -528,7 +397,7 @@ struct LocationCardView: View {
                                     let firstChar = Array(review.userID)[0]
                                     EvenNewerReviewView(review: review, firstChar: String(firstChar).uppercased())
                                 }
-                            }.scrollContentBackground(.hidden)
+                            }.scrollContentBackground(.hidden).padding(.top, -20)
                             Spacer()
                             Spacer()
                         }
@@ -560,7 +429,7 @@ struct LocationCardView: View {
             .shadow(radius: 10)
             .padding()
             .sheet(isPresented: $showingWriteReviewSheet) {
-                WriteReviewView(isPresented: $showingWriteReviewSheet) { rating, title, text in
+                WriteReviewView(viewModel: LocationCardViewModel(college: viewModel.college, location: viewModel.location), isPresented: $showingWriteReviewSheet) { rating, title, text in
                     viewModel.submitReview(rating: rating, title: title, text: text, forLocation: viewModel.location.id)
                 }
                 .presentationDetents([.fraction(1)])
@@ -659,8 +528,8 @@ struct MapView: View {
 
 
 // Map annotation view goes here (if using MapKit)
-
 struct WriteReviewView: View {
+    @StateObject var viewModel: LocationCardViewModel
     @Binding var isPresented: Bool
     @State private var rating: Int = 0
     @State private var titleText: String = ""
@@ -668,60 +537,80 @@ struct WriteReviewView: View {
     var onSubmit: (Int, String, String) -> Void
 
     var body: some View {
-        NavigationView {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [colorForCategory(viewModel.location.category), Color.white]), startPoint: .topLeading, endPoint: .bottomTrailing)
             VStack {
                 ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.white)
+                        .padding(.horizontal, 20)
+                        .frame(height: 100)
                     HStack {
-                        Spacer()
+                        Image(viewModel.college.image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 80, height: 80)
+                            .clipped()
+                            .cornerRadius(10)
+                        VStack(alignment: .leading) {
+                            Text(viewModel.location.name)
+                                .font(.title)
+                            Text("\(viewModel.college.name) - \(viewModel.college.city)")
+                                .font(.headline)
+                        }
                     }
-                    HStack {
-                        Text("Write review")
-                            .font(.title
-                                .bold())
-                    }
-                    HStack {
-                        Spacer()
-                        Button("") {
+                }
+                .padding(.top, 20)
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.white)
+                        .padding(.horizontal, 20)
+                    VStack {
+                        HStack {
+                            ForEach(0..<5) {value in
+                                Image(systemName: "star.fill")
+                                    .resizable()
+                                    .frame(width: 30,height: 30)
+                                    .foregroundColor(self.rating >= value ? .yellow : .gray)
+                                    .onTapGesture {
+                                        self.rating = value
+                                    }
+                            }
+                        }
+                        VStack(alignment: .leading) {
+                            Text("Title your review")
+                                .font(.headline)
+                            TextField("Title", text: $titleText)
+                                .border(Color.black)
+                            Text("Tell us your thoughts")
+                                .font(.headline)
+                            TextEditor(text: $reviewText)
+                                .border(Color.black)
+                        }
+                        Button("Submit") {
+                            onSubmit(rating + 1, titleText, reviewText)
                             isPresented = false
                         }
-                            .buttonStyle(xButton())
-                            .shadow(radius: 10)
-                        
+                        .frame(width: 160, height: 60)
+                        .background(Color.black)
+                        .foregroundColor(.white)
+                        .cornerRadius(40)
+                        .padding()
                     }
+                    .padding(30)
+                    
                 }
-                .padding()
-                HStack {
-                    ForEach(0..<5) {value in
-                        Image(systemName: "star.fill")
-                            .resizable()
-                            .frame(width: 30,height: 30)
-                            .foregroundColor(self.rating >= value ? .yellow : .gray)
-                            .onTapGesture {
-                                self.rating = value
-                            }
-                    }
-                }
-                TextEditor(text: $titleText)
-                    .frame(minHeight: 20)
-                    .border(Color.black)
-                    .padding()
-                
-                TextEditor(text: $reviewText)
-                    .frame(minHeight: 200)
-                    .border(Color.black)
-                    .padding()
-                   
-                
-                Button("Submit") {
-                    onSubmit(rating + 1, titleText, reviewText)
-                    isPresented = false
-                }
-                .frame(width: 160, height: 60)
-                .background(Color.blue.opacity(0.5))
-                .foregroundColor(.white)
-                .cornerRadius(40)
-                .padding()
+                Spacer()
             }
+        }
+        .overlay(alignment: .topTrailing) {
+            Button("") {
+                isPresented = false
+                // this should dismiss the sheet
+            }
+            .buttonStyle(xButton())
+            .shadow(radius: 10)
+            .padding(10)
         }
     }
 }
