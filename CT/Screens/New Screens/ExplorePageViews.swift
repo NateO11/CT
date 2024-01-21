79fd7dc89ec686297014e206dfdb3267cc45ade6
griffin-and-co <1griffinharrison@gmail.com>
2024-01-21 11:44:42 -0500

    pop up display for schools not fully built - coming soon message

--- CT/Screens/New Screens/ExplorePageViews.swift
+++ CT/Screens/New Screens/ExplorePageViews.swift
@@ -68,6 +68,7 @@ struct LargeImageSection: View {
 
 struct SchoolScrollView: View {
     var colleges: [College]
+    @State private var showAlert: Bool = false
     var body: some View {
         GeometryReader(content: { geometry in
             let size = geometry.size
@@ -75,7 +76,34 @@ struct SchoolScrollView: View {
             ScrollView(.horizontal) {
                 HStack(spacing: 5) {
                     ForEach(colleges) { college in
-                        NavigationLink(destination: SchoolView(viewModel: CollegeDetailViewModel(college: college))) {
+                        if college.available {
+                            NavigationLink(destination: SchoolView(viewModel: CollegeDetailViewModel(college: college))) {
+                                GeometryReader(content: { proxy in
+                                    let cardSize = proxy.size
+                                    let minX = proxy.frame(in: .scrollView).minX - 60
+                                    // let minX = min(((proxy.frame(in: .scrollView).minX - 60) * 1.4), size.width * 1.4)
+                                        
+                                    Image(college.image)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .offset(x: -minX)
+                                        .frame(width: proxy.size.width * 1.5)
+                                        .frame(width: cardSize.width, height: cardSize.height)
+                                        .overlay {
+                                            OverlayView(college, available: college.available)
+                                        }
+                                        .clipShape(.rect(cornerRadius: 15))
+                                        .shadow(color: .black.opacity(0.25), radius: 8, x: 5, y: 10)
+                                    
+                                })
+                                .frame(width: size.width - 120, height: size.height - 30)
+                                .scrollTransition(.interactive, axis: .horizontal) {
+                                    view, phase in
+                                    view
+                                        .scaleEffect(phase.isIdentity ? 1 : 0.95)
+                            }
+                            }
+                        } else {
                             GeometryReader(content: { proxy in
                                 let cardSize = proxy.size
                                 let minX = proxy.frame(in: .scrollView).minX - 60
@@ -87,11 +115,23 @@ struct SchoolScrollView: View {
                                     .offset(x: -minX)
                                     .frame(width: proxy.size.width * 1.5)
                                     .frame(width: cardSize.width, height: cardSize.height)
+                                    .blur(radius: 2)
                                     .overlay {
                                         OverlayView(college, available: college.available)
                                     }
                                     .clipShape(.rect(cornerRadius: 15))
                                     .shadow(color: .black.opacity(0.25), radius: 8, x: 5, y: 10)
+                                    .onTapGesture {
+                                        showAlert = true
+                                    }
+                                    .alert(isPresented: $showAlert) {
+                                        Alert(
+                                            title: Text("School unavailable"),
+                                            message: Text("\(college.name) \n is coming soon!"),
+                                            dismissButton: .default(Text("OK"))
+                                        )
+                                    }
+
                                 
                             })
                             .frame(width: size.width - 120, height: size.height - 30)
@@ -99,7 +139,7 @@ struct SchoolScrollView: View {
                                 view, phase in
                                 view
                                     .scaleEffect(phase.isIdentity ? 1 : 0.95)
-                        }
+                            }
                         }
                     }
                 }
@@ -144,20 +184,37 @@ struct SchoolScrollView: View {
                 .padding(20)
             })
         } else {
-            ZStack {
-                Color.gray.opacity(0.7)
+            ZStack(alignment: .bottomLeading) {
+                LinearGradient(colors: [
+                    Color.gray,
+                    .gray.opacity(0.8),
+                    .gray.opacity(0.6),
+                    .gray.opacity(0.8),
+                    .gray
+                ], startPoint: .top, endPoint: .bottom)
                 VStack {
-                    Text(college.name)
-                        .font(.title)
-                        .multilineTextAlignment(.center)
-                        .fontWeight(.bold)
-                        .foregroundStyle(.white)
-                        .padding(.bottom, 20)
-                    Text("Coming Soon")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Text("Coming Soon!")
+                            .font(.title)
+                            .fontWeight(.black)
+                            .foregroundStyle(.white)
+                        Spacer()
+                    }
+                    Spacer()
                 }
+                VStack(alignment: .leading, spacing: 4, content: {
+                    Text(college.name)
+                        .font(.title2)
+                        .multilineTextAlignment(.leading)
+                        .fontWeight(.black)
+                        .foregroundStyle(.white.opacity(0.8))
+                    Text(college.city)
+                        .font(.callout)
+                        .foregroundStyle(.white.opacity(0.8))
+                })
+                .padding(20)
             }
         }
         
