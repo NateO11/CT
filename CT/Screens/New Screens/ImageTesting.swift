4622237abd71bff9e162229ff44cdc4f4a2752e8
NateO11 <ndo1503@gmail.com>
2024-02-11 15:31:39 -0500

    Added the splash screen animation

---
+++ CT/Screens/New Screens/ImageTesting.swift
@@ -0,0 +1,18 @@
+//
+//  ImageTesting.swift
+//  CT
+//
+//  Created by Nate Owen on 2/11/24.
+//
+
+import SwiftUI
+
+struct ImageTesting: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageTesting()
+}
---
+++ CT/Screens/New Screens/littlepraccy.swift
@@ -0,0 +1,18 @@
+//
+//  littlepraccy.swift
+//  CT
+//
+//  Created by Nate Owen on 2/11/24.
+//
+
+import SwiftUI
+
+struct littlepraccy: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    littlepraccy()
+}
