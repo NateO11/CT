fdcf800ea0dec6c490de92e447dd79f5c3f0afe3
NateO11 <ndo1503@gmail.com>
2024-02-21 12:19:35 -0500

    added new images and changed the sliding splashscreen

--- CT/Screens/Authentication/Login/SplashScreenView.swift
+++ CT/Screens/Authentication/Login/SplashScreenView.swift
@@ -12,36 +12,46 @@ import SwiftUI
 struct SplashScreenView: View {
     @State private var scale: CGFloat = 1
     @State private var opacity: Double = 1
+   // @State private var scale: Double = 1
     
     
     var body: some View {
         
         ZStack {
-                    Rectangle()
-                        .fill(Color.white)
-                        .ignoresSafeArea(.all)
-                        .onAppear {
-                            withAnimation(Animation.easeInOut(duration: 3).delay(1)) {
-                                self.opacity = 0.0
-                            }
-                        }
-                    
-                    Image("CTlogo")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 100, height: 100)
-                        .cornerRadius(20)
-                        .scaleEffect(scale)
-                        .opacity(opacity)
-                        .onAppear {
-                            withAnimation(Animation.easeInOut(duration: 2).delay(1)) {
-                                self.scale = 20.0
-                                self.opacity = 0.0
-                            }
+            Rectangle()
+                .fill(Color.white)
+                .ignoresSafeArea(.all)
+                .onAppear {
+                    withAnimation(Animation.easeInOut(duration: 3).delay(1)) {
+                        self.opacity = 0.0
+                    }
+                }
+            
+            Image("CTMap")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 300)
+                .cornerRadius(20)
+                .scaleEffect(scale)
+                .opacity(opacity)
+            
+                Image("CTPin")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200)
+                    .cornerRadius(20)
+                    .offset(x: 0, y: -90)
+                    .scaleEffect(scale)
+                    .opacity(opacity)
+                    .onAppear {
+                        withAnimation(.spring()) {
+                            // Update any animatable property here
+                            self.scale = 1.2
                         }
                 }
-            }
         }
+    }
+}
     
 
 
