0d1f3e5719ba9a69b36d146b4b8ef681a6042310
NateO11 <ndo1503@gmail.com>
2024-03-08 18:13:37 -0500

    error handling with the login and signup feature

---
+++ CT/Screens/Authentication/AuthViews/practice imageretriaval.swift
@@ -0,0 +1,18 @@
+//
+//  practice imageretriaval.swift
+//  CT
+//
+//  Created by Nate Owen on 3/8/24.
+//
+
+import SwiftUI
+
+struct practice_imageretriaval: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    practice_imageretriaval()
+}
---
+++ CT/Screens/New Screens/practice.swift
@@ -0,0 +1,18 @@
+//
+//  practice.swift
+//  CT
+//
+//  Created by Nate Owen on 3/8/24.
+//
+
+import SwiftUI
+
+struct practice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    practice()
+}
